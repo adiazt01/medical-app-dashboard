62ce6e7b8f99f4826c443f7b4c32b91c046371d3
adiazt01 <adiaztovar2004@gmail.com>
2025-01-13 21:13:46 -0400

    refactor(product-table): simplify column definitions and improve header structure

--- src/modules/products/components/table/columns.tsx
+++ src/modules/products/components/table/columns.tsx
@@ -3,95 +3,88 @@ import { ColumnDef } from "@tanstack/react-table"
 import { Book, Camera, DollarSign, Hash, Heart, Info, PillBottle, Tablets } from "lucide-react"
 
 export type product = {
-    id: number
+  id: number
+  name: string
+  price: number
+  description: string
+  createdAt: string
+  updatedAt: string
+  laboratory: {
     name: string
-    price: number
-    description: string
-    createdAt: string
-    updatedAt: string
-    laboratory: {
-        name: string
-    },
-    mainComponent: {
-        name: string
-    },
-    therapeuticAction: {
-        name: string
-    },
-    presentation: {
-        name: string
-    },
-    file: {
-        url: string
-    }
+  },
+  mainComponent: {
+    name: string
+  },
+  therapeuticAction: {
+    name: string
+  },
+  presentation: {
+    name: string
+  },
+  file: {
+    url: string
+  }
 }
 
 export const columns: ColumnDef<product>[] = [
-    {
-        header: () => <div className="flex flex-row text-center items-center justify-center w-full gap-2 ">
-            <Camera size={14} />
-            <p className="mr-2">
+  {
+    header: () => <div>
+      <p className="mr-2">
 
-                Imagen
-            </p>
-        </div>,
-        cell: ({ row }) => {
-            return (<AspectRatio ratio={16 / 9}>
-                <img src={row.original.file.url} alt="product" className="mx-auto rounded-md object-cover" />
-            </AspectRatio>)
-        },
-        accessorKey: 'file.url',
+        Imagen
+      </p>
+    </div>,
+    cell: ({ row }) => {
+      return (<AspectRatio ratio={16 / 9}>
+        <img src={row.original.file.url} alt="product" className="mx-auto rounded-md object-cover" />
+      </AspectRatio>)
     },
-    {
-        header: () => <div className="flex flex-row items-center gap-1">
-            <PillBottle size={14} />
-            Nombre
-        </div>,
-        accessorKey: 'name',
-        cell: ({ cell }) => {
-            return <div className="truncate">{cell.getValue()}</div>
-        }
+    accessorKey: 'file.url',
+  },
+  {
+    header: () => <div>
+      Nombre
+    </div>,
+    accessorKey: 'name',
+    cell: ({ cell }) => {
+      return <div className="truncate">{cell.getValue()}</div>
+    }
+  },
+  {
+    header: () => <div>
+      Precio
+    </div>,
+    accessorKey: 'price',
+  },
+  {
+    header: () => <div>
+      Descripcion
+    </div>,
+    accessorKey: 'description',
+  },
+  {
+    header: () => <div>
+      Laboratorio
+    </div>,
+    cell: ({ cell }) => {
+      return <div className="truncate">{cell.getValue()}</div>
     },
-    {
-        header: () => <div className="flex flex-row items-center gap-1">
-            <DollarSign size={14} />
-            Precio
-        </div>,
-        accessorKey: 'price',
+    accessorKey: 'laboratory.name',
+  },
+  {
+    header: () => <div>
+      Componente principal
+    </div>,
+    cell: ({ cell }) => {
+      return <div className="truncate">{cell.getValue()}</div>
     },
-    {
-        header: () => <div className="flex flex-row items-center gap-1">
-            <Book size={14} />
-            Descripcion
-        </div>,
-        accessorKey: 'description',
-    },
-    {
-        header: () => <div className="flex flex-row items-center gap-1">
-            <Info size={14} />
-            Laboratorio
-        </div>,
-        cell: ({ cell }) => {
-            return <div className="truncate">{cell.getValue()}</div>
-        },
-        accessorKey: 'laboratory.name',
-    },
-    {
-        header: () => <div className="flex truncate flex-row items-center gap-1">
-            <Tablets size={14} />
-            Componente principal
-        </div>,
-        cell: ({ cell }) => {
-            return <div className="truncate">{cell.getValue()}</div>
-        },
-        accessorKey: 'mainComponent.name',
-    },
-    {
-        header: () => <div className="flex flex-row items-center gap-1">
-            <Heart size={14} />
-            Accion terapeutica
-        </div>,
-        accessorKey: 'therapeuticAction.name',
-    },
+    accessorKey: 'mainComponent.name',
+  },
+  {
+    header: () => <div className="truncate">
+      Accion terapeutica
+    </div>,
+    accessorKey: 'therapeuticAction.name',
+  },
 ]
 
